930ca2184c7af7d276d2a0ac7ac159749a32d63e
Lucas Voboril <lucas.voboril@wolox.com.ar>
2018-05-04 08:54:30 -0300

    Reset -1 becouse last commit omitted confirmation

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :user
+  mount_devise_token_auth_for 'User', at: 'auth'
+  resources :users
+  get 'home/index'
+
+  get 'user/index'
+  
+  root to: "home#index"
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
