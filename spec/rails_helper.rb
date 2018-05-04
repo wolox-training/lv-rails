48077d38dd8cf9982c0726779947d2c657567cdb
Lucas Voboril <lucas.voboril@wolox.com.ar>
2018-05-04 12:26:44 -0300

    Initial configuration of rspec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,7 +6,11 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
+<<<<<<< HEAD
 require 'support/factory_bot'
+=======
+
+>>>>>>> 271ba69... Initial configuration of rspec
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
