b11f348a1b059c426a74c6a040677d52d0552a34
Lucas Voboril <lucas.voboril@wolox.com.ar>
2018-05-04 13:30:22 -0300

    Incorrect use of ENV variables: corrected

--- config/application.rb
+++ config/application.rb
@@ -5,15 +5,6 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-Bundler.require(:default, Rails.env)
-
-if File.exists?(File.expand_path('../secrets.yml', __FILE__))
-  config = YAML.load(File.read(File.expand_path('../secrets.yml', __FILE__)))
-  config.merge! config.fetch(Rails.env, {})
-  config.each do |key, value|
-    ENV[key] ||= value.to_s unless value.kind_of? Hash
-  end
-end
 
 module WBooks
   class Application < Rails::Application
