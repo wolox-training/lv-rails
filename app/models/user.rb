9b92383673d3aac17497da4b3ecba551e2501630
Lucas Voboril <lucas.voboril@wolox.com.ar>
2018-05-07 10:39:17 -0300

    Parcial fix for user test

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   include DeviseTokenAuth::Concerns::User
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 end
