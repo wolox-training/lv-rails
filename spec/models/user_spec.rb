12ae24652c4c1d39a89d02281682fd281f99f172
Lucas Voboril <lucas.voboril@wolox.com.ar>
2018-05-07 10:39:17 -0300

    Parcial fix for user test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,56 +2,53 @@ require 'rails_helper'
 
 describe User do
   
-  # context '#attr_getters' do
-  #   it do
-  #     should validate_presence_of(:first_name)
-  #   end
-  #   it do
-  #     should validate_presence_of(:last_name)
-  #   end
-  # end
+  subject(:user) do
+    User.new(
+      first_name: first_name, last_name: last_name, email: email, password: password,
+      password_confirmation: password_confirmation
+    )
+  end
 
-  # subject(:user) do
-  #   User.new(
-  #     first_name: first_name, last_name: last_name, email: email, password: password,
-  #     password_confirmation: password_confirmation
-  #   )
-  # end
+  let(:first_name)            { Faker::Name.first_name }
+  let(:last_name)             { Faker::Name.last_name }
+  let(:email)                 { Faker::Internet.email }
+  let(:password)              { 'password' }
+  let(:password_confirmation) { 'password' }
 
-  # let(:first_name)            { Faker::Name.first_name }
-  # let(:last_name)             { Faker::Name.last_name }
-  # let(:email)                 { Faker::Internet.email }
-  # let(:password)              { 'password' }
-  # let(:password_confirmation) { 'password' }
+  context 'With an empty user' do
+    it '#attr_getters' do
+      %w[first_name last_name].each do |unAtr|
+        expect(user.attributes.keys).to include(unAtr)
+      end
+    end
 
-  # it do
-  #   is_expected.to be_valid
-  # end
+    it do
+      is_expected.to be_valid
+    end
+  end
 
   # describe '#generate_verification_code' do
   #   context 'When the user is created' do
   #     it 'generates a random verification code' do
-  #       user.save!
-  #       expect(user.verification_code).to be_present
+  #       anUser.save!
+  #       expect(anUser.verification_code).to be_present
   #     end
   #   end
   # end
 
-  # describe '#create' do
-  #   context 'When the first_name is nil' do
-  #     let(:first_name) { nil }
+  describe '#create' do
+    context 'When the first_name is nil' do
+      let(:first_name) { nil }
+      it do
+        is_expected.to be_invalid
+      end
+    end
 
-  #     it do
-  #       is_expected.to be_invalid
-  #     end
-  #   end
-
-  #   context 'When the last_name is nil' do
-  #     let(:last_name) { nil }
-
-  #     it do
-  #       is_expected.to be_invalid
-  #     end
-  #   end
-  # end
+    context 'When the last_name is nil' do
+      let(:last_name) { nil }
+      it do
+        is_expected.to be_invalid
+      end
+    end
+  end
 end
