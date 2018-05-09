9e7a17e66963ce5e2989947f50bb896aca86d9bb
Lucas Voboril <lucas.voboril@wolox.com.ar>
2018-05-09 09:51:44 -0300

    Delete routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,3 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
-  
-  get 'home/index'
-  get 'user/index'
-  
-  root to: "home#index"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
