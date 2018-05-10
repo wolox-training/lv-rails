f24058a42f275084fdc3a5d9fc7185b75bd43185
Lucas Voboril <lucas.voboril@wolox.com.ar>
2018-05-10 10:56:28 -0300

    It's not necesary to add null constraint in change_table method

--- db/migrate/20180509133508_add_null_constraint_to_book.rb
+++ db/migrate/20180509133508_add_null_constraint_to_book.rb
@@ -1,12 +1,10 @@
 class AddNullConstraintToBook < ActiveRecord::Migration[5.1]
   def change
-    change_table(:books) do
-      change_column_null :books, :genre, false
-      change_column_null :books, :author, false
-      change_column_null :books, :image, false
-      change_column_null :books, :title, false
-      change_column_null :books, :publisher, false
-      change_column_null :books, :year, false
-    end
+    change_column_null :books, :genre, false
+    change_column_null :books, :author, false
+    change_column_null :books, :image, false
+    change_column_null :books, :title, false
+    change_column_null :books, :publisher, false
+    change_column_null :books, :year, false
   end
 end
