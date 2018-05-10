c4a117c8f70dc6a856c774e625a0934120296197
Lucas Voboril <lucas.voboril@wolox.com.ar>
2018-05-10 13:21:56 -0300

    Rubocop autofix conflicts

--- spec/models/rent_spec.rb
+++ spec/models/rent_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe Rent do
-  # To execute this test I need to have approved 
+  # To execute this test I need to have approved
   # previous PRs, becouse in this branch i had a bad
   # bad configuration to execute rspec
 end
