4158f70affa3edd8b13a99e10af2177faf649af0
Lucas Voboril <lucas.voboril@wolox.com.ar>
2018-05-10 10:57:39 -0300

    Rent migration: it's not necesary null constraint in change_table method

--- db/migrate/20180510133218_add_constraint_null_to_rent.rb
+++ db/migrate/20180510133218_add_constraint_null_to_rent.rb
@@ -1,10 +1,8 @@
 class AddConstraintNullToRent < ActiveRecord::Migration[5.1]
   def change
-    change_table(:rents) do
-      change_column_null :rents, :user, false
-      change_column_null :rents, :book, false
-      change_column_null :rents, :from, false
-      change_column_null :rents, :to, false
-    end
+    change_column_null :rents, :user, false
+    change_column_null :rents, :book, false
+    change_column_null :rents, :from, false
+    change_column_null :rents, :to, false
   end
 end
