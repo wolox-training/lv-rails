d728cf118b2733a47d84adeb2483bc035c134698
Lucas Voboril <lucas.voboril@wolox.com.ar>
2018-05-11 10:38:49 -0300

    Problem with include Filterable module for bad name

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,6 @@
 class Book < ApplicationRecord
+  include Filterable
+
   validates :genre, :author, :image, :title, :publisher, :year, presence: true
 
   has_one :rent, dependent: :destroy
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
 
   resources :book, only: [:show, :index]
+
+  get '/books' => 'book#index'
 end
