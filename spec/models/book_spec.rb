1e1117fc704efb83fbbba15edef098bb08219760
Lucas Voboril <lucas.voboril@wolox.com.ar>
2018-05-11 13:36:29 -0300

    Implementing correctly shoulda_matchers

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -4,12 +4,9 @@ describe Book do
   subject(:book) { build(:book) }
 
   it { is_expected.to be_valid }
-
-  context 'validates not null fields' do
-    it { book.validates_presence_of :genre }
-    it { book.validates_presence_of :author }
-    it { book.validates_presence_of :image }
-    it { book.validates_presence_of :publisher }
-    it { book.validates_presence_of :year }
-  end
+  it { is_expected.to validate_presence_of(:genre) }
+  it { is_expected.to validate_presence_of(:author) }
+  it { is_expected.to validate_presence_of(:image) }
+  it { is_expected.to validate_presence_of(:publisher) }
+  it { is_expected.to validate_presence_of(:year) }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,9 +4,6 @@ describe User do
   subject(:user) { build(:user) }
 
   it { is_expected.to be_valid }
-
-  context 'validates not null fields' do
-    it { user.validates_presence_of :first_name }
-    it { user.validates_presence_of :last_name }
-  end
+  it { is_expected.to validate_presence_of :first_name }
+  it { is_expected.to validate_presence_of :last_name }
 end
