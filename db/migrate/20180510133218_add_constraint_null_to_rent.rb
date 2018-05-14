faab6784451e1bdc2217535fd257f48780d49e78
Lucas Voboril <lucas.voboril@wolox.com.ar>
2018-05-14 10:05:43 -0300

    Change variable name in migrate

--- db/migrate/20180510133218_add_constraint_null_to_rent.rb
+++ db/migrate/20180510133218_add_constraint_null_to_rent.rb
@@ -1,7 +1,7 @@
 class AddConstraintNullToRent < ActiveRecord::Migration[5.1]
   def change
-    change_column_null :rents, :user, false
-    change_column_null :rents, :book, false
+    change_column_null :rents, :user_id, false
+    change_column_null :rents, :book_id, false
     change_column_null :rents, :from, false
     change_column_null :rents, :to, false
   end
