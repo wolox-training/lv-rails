646ea2db1bb76faab8f305c412c7abb142c6775b
Lucas Voboril <lucas.voboril@wolox.com.ar>
2018-05-14 10:31:58 -0300

    Add validation date from less than date to in rent model

--- app/models/rent.rb
+++ app/models/rent.rb
@@ -6,4 +6,10 @@ class Rent < ApplicationRecord
   validates :book, presence: true
   validates :from, presence: true
   validates :to, presence: true
+
+  validate :validate_date_from_is_less_than_date_to
+
+  def validate_date_from_is_less_than_date_to
+    errors.add(:to, 'Date from must be less than date to') if from && to && to < from
+  end
 end
