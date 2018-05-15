129cbd17e6e8ead370269958f2d9d3ac87613e8b
Lucas Voboril <lucas.voboril@wolox.com.ar>
2018-05-15 11:14:40 -0300

    Testing book controller without template

--- app/controllers/book_controller.rb
+++ app/controllers/book_controller.rb
@@ -1,6 +1,8 @@
 class BookController < ApplicationController
   include Wor::Paginate
 
+  before_action :authenticate_user!, except: %i[show index]
+
   def index
     @books = Book.filter(params.require(:book).permit(%i[genre title author]))
     render_paginated @books, each_serializer: BookSerializer
