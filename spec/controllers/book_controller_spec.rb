f8d14a911c9ad990f579b1823bbe5cbed103fc6e
Lucas Voboril <lucas.voboril@wolox.com.ar>
2018-05-15 12:32:51 -0300

    Testing index with token auth

--- spec/controllers/book_controller_spec.rb
+++ spec/controllers/book_controller_spec.rb
@@ -1,15 +1,68 @@
 require 'rails_helper'
 
 describe BookController do
-  login_user
+  let!(:books) { create_list(:book, 3) }
 
-  it 'get index' do
-    get 'index', params: { book: { author: 'King' } }
-    expect(response).to be_success
+  describe 'GET #index' do
+    context 'Unusual params' do
+      it 'without params' do
+        expect { get :index }.to raise_error ActionController::ParameterMissing
+      end
+
+      it 'with an empty book param' do
+        expect { get :index, params: { book: {} } }.to raise_error ActionController::ParameterMissing
+      end
+
+      it 'adding additional params' do
+        b = books.first
+        get :index, params: { book: {
+          genre: b.genre,
+          title: b.title,
+          author: b.author,
+          # Below are additional params
+          image: b.image,
+          publisher: b.publisher,
+          year: b.year
+        } }
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    before do
+      b = books.first
+      get :index, params: { book: {
+        genre: b.genre,
+        title: b.title,
+        author: b.author
+      } }
+    end
+
+    context 'With an authenticated user' do
+      include_context 'Authenticated User'
+
+      it 'returns a 200 ok status' do
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    # context 'With an unauthenticated user' do
+    #   it 'returns an unauthorized status' do
+    #     expect(response).to have_http_status(:unauthorized)
+    #   end
+    # end
   end
 
-  it 'get show' do
-    get 'show', params: { book: { id: 1 } }
-    expect(response).to be_success
-  end
+  # describe 'GET #show' do
+  #   it 'Without params' do
+  #     expect { get :show }.to raise_error ActionController::ParameterMissing
+  #   end
+
+  #   before do
+  #     get :show, params: { book: { id: books.first.id } }
+  #   end
+
+  #   it 'responds with 200 status' do
+  #     expect(response).to have_http_status(:ok)
+  #   end
+  # end
 end
