9a15b054d290e420bae141b7454d5106e3c5f185
Lucas Voboril <lucas.voboril@wolox.com.ar>
2018-05-16 12:34:39 -0300

    I deleted @ in controllers

--- app/controllers/book_controller.rb
+++ app/controllers/book_controller.rb
@@ -4,12 +4,12 @@ class BookController < ApplicationController
   before_action :authenticate_user!, only: %i[show index]
 
   def index
-    @books = Book.filter(params.require(:book).permit(%i[genre title author]))
-    render_paginated @books, each_serializer: BookSerializer
+    books = Book.filter(params.require(:book).permit(%i[genre title author]))
+    render_paginated books, each_serializer: BookSerializer
   end
 
   def show
-    @book = Book.filter(params.require(:book).permit(:id))
-    render_paginated @book
+    book = Book.filter(params.require(:book).permit(:id))
+    render_paginated book
   end
 end
--- app/controllers/rent_controller.rb
+++ app/controllers/rent_controller.rb
@@ -4,12 +4,12 @@ class RentController < ApplicationController
   before_action :authenticate_user!, only: %i[create index]
 
   def create
-    @rent = Rent.create(params[:tweet])
-    render json: @rent, status: 201
+    rent = Rent.create(params[:tweet])
+    render json: rent, status: 201
   end
 
   def index
-    @rents = Rent.filter(params.require(:rent).permit(%i[user book from to]))
-    render_paginated @rents, each_serializer: RentSerializer
+    rents = Rent.filter(params.require(:rent).permit(%i[user book from to]))
+    render_paginated rents, each_serializer: RentSerializer
   end
 end
