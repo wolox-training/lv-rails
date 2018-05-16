ace609139b77f50116f4de1d133f26651eb6ea11
Lucas Voboril <lucas.voboril@wolox.com.ar>
2018-05-16 11:59:12 -0300

    Incorporing test for rent controller

--- spec/controllers/rent_controller_spec.rb
+++ spec/controllers/rent_controller_spec.rb
@@ -1,29 +1,36 @@
 require 'rails_helper'
 
 describe RentController do
-
-  describe 'GET #index' do
-
-    def index_requiere
-      r = rents.first
-      get :index, params: { rent: {
-        user: r.user,
-        book: r.book,
-        from: r.from,
-        to:   r.to
-      } }
-    end
-
+  describe 'POST #create' do
     context 'With an unauthenticated user' do
       it 'returns an unauthorized status' do
-        index_requiere
+        post :create, params: {}
         expect(response).to have_http_status(:unauthorized)
       end
     end
 
     context 'With an authenticated user' do
       include_context 'Authenticated User'
-      let!(:rents) { create_list(:book, 3, user: user) }
+
+      it 'returns a 204 ok status' do
+        # 204: The server has successfully fulfilled the request and that
+        # there is no additional content to send in the response payload body.
+        post :create, params: { rent: create(:rent) }
+        expect(response).to have_http_status(:no_content)
+      end
+    end
+  end
+
+  describe 'GET #index' do
+    context 'With an unauthenticated user' do
+      it 'returns an unauthorized status' do
+        get :index, params: {}
+        expect(response).to have_http_status(:unauthorized)
+      end
+    end
+
+    context 'With an authenticated user' do
+      include_context 'Authenticated User'
 
       it 'without params' do
         expect { get :index }.to raise_error ActionController::ParameterMissing
@@ -33,17 +40,20 @@ describe RentController do
         expect { get :index, params: { rent: {} } }.to raise_error ActionController::ParameterMissing
       end
 
+      def index_requiere
+        r = create(:rent)
+        get :index, params: { rent: {
+          user: r.user,
+          book: r.book,
+          from: r.from,
+          to:   r.to
+        } }
+      end
+
       it 'returns a 200 ok status' do
         index_requiere
         expect(response).to have_http_status(:ok)
       end
-    end
-  end
-
-  describe 'GET #create' do
-    it 'returns http success' do
-      post :create, params: { rent: rents.first }
-      expect(response).to have_http_status(:success)
     end
   end
 end
