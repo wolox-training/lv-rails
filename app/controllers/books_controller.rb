2022fe2dbcfdaa1125098805454ed48f03e72a4e
Lucas Voboril <lucas.voboril@wolox.com.ar>
2018-05-22 12:56:20 -0300

    I incorporate PR suggestions in book_controller_spec

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,6 +3,8 @@ class BooksController < ApiController
 
   before_action :authenticate_user!, only: %i[show index]
 
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
   def index
     books = if params['book'].present?
               Book.filter(params.require(:book).permit(%i[genre title author]))
@@ -15,5 +17,11 @@ class BooksController < ApiController
   def show
     book = Book.find(params[:id])
     render json: book, status: 200
+  end
+
+  protected
+
+  def record_not_found
+    render json: { error: 'Record not found' }, status: :not_found
   end
 end
--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -4,52 +4,45 @@ describe BooksController do
   let!(:books) { create_list(:book, 3) }
 
   describe 'GET #index' do
-    context 'With an unauthenticated user' do
+    context 'with an unauthenticated user' do
       it 'returns an unauthorized status' do
         get :index, params: {}
         expect(response).to have_http_status(:unauthorized)
       end
     end
 
-    context 'With an authenticated user' do
+    context 'with an authenticated user' do
       include_context 'Authenticated User'
 
-      it 'without params returns all books' do
-        get :index
-        expect(JSON.parse(response.body)['total_count']).to eq(books.size)
+      context 'with a correct param' do
+        it 'returns a 200 ok status' do
+          get :index, params: { book: { genre: books.first.genre } }
+          expect(response).to have_http_status(:ok)
+        end
       end
 
-      it 'returns a 200 ok status' do
-        get :index, params: { book: { genre: books.first.genre } }
-        expect(response).to have_http_status(:ok)
+      context 'without params' do
+        it 'returns all books' do
+          get :index
+          expect(JSON.parse(response.body)['total_count']).to eq(books.size)
+        end
       end
 
-      def unique_title
-        rnd_title = Faker::Book.title
-        rnd_title = Faker::Book.title while books.map(&:title).include?(rnd_title)
-        rnd_title
+      context 'with inexistent filters' do
+        it 'returns empty response' do
+          get :index, params: { book: { title: 'HY43Ewcqw!$ds1' } }
+          expect(JSON.parse(response.body)['total_count']).to eq(0)
+        end
       end
 
-      it 'params filters not exists' do
-        get :index, params: { book: { title: unique_title } }
-        expect(JSON.parse(response.body)['total_count']).to eq(0)
-      end
-
-      def full_index_response
-        b = books.first
-        get :index, params: { book: {
-          title: b.title,
-          # Below are additional params
-          image: b.image,
-          publisher: b.publisher,
-          year: b.year
-        } }
-      end
-
-      it 'setting additionals params' do
-        full_index_response
-        expect(response).to have_http_status(:ok)
-        # it response status ok becouse additionals params are not used
+      context 'with additionals params' do
+        it 'additionals params are not used' do
+          b = books.first
+          get :index, params: { book: { title: b.title,
+                                        image: b.image, publisher: b.publisher, year: b.year } }
+          book_response = Book.new(JSON.parse(response.body)['page'][0])
+          expect(book_response.title).to be_eql(books.first.title)
+        end
       end
     end
   end
@@ -70,19 +63,11 @@ describe BooksController do
         expect(response).to have_http_status(:ok)
       end
 
-      it 'shows only 1 book' do
-        get :show, params: { id: books.first.id }
-        expect(JSON.parse(response.body).size).to eq(1)
-      end
-
-      def unique_id
-        rnd_id = Faker::Number.number(10)
-        rnd_id = Faker::Number.number(10) while books.map(&:id).include?(rnd_id)
-        rnd_id
-      end
-
-      it 'With an incorrect id' do
-        expect { get :show, params: { id: unique_id } }.to raise_error ActiveRecord::RecordNotFound
+      context 'With an incorrect id' do
+        it 'responds with 404 status' do
+          get :show, params: { id: '-1' }
+          expect(response).to have_http_status(:not_found)
+        end
       end
     end
   end
