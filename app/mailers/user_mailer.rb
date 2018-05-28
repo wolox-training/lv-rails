6e15e37ab87eba16b972ed44affd43f536919d94
Lucas Voboril <lucas.voboril@wolox.com.ar>
2018-05-28 12:34:24 -0300

    Initial config user_mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,21 +1,10 @@
 class UserMailer < ApplicationMailer
-  default domain: Rails.application.secrets.user_mail_domain,
-          password: Rails.application.secrets.user_mail_password,
-          user_name: Rails.application.secrets.user_mail_user_name,
-          address: Rails.application.secrets.user_mail_address,
-          port: Rails.application.secrets.user_mail_port,
-          authentication: Rails.application.secrets.user_mail_authentication.to_sym
+  def new_rent_notification(rent)
+    @rent = rent
+    @user = User.find(rent.user_id)
+    @book = Book.find(rent.book_id)
 
-  def new_record_notification(record)
-    # The email data should be:
-
-    # Actual date of rent creation
-    # Book detail
-    # Date from and date to of the rent
-    # El email debe estar maquetado
-
-    @record = record
-    mail to: 'recipient@MYDOMAIN.com',
-         subject: 'Success! You did it.'
+    mail to: 'lucas.voboril@wolox.com.ar',
+         subject: 'New rent notification'
   end
 end
