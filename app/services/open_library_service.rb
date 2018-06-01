134006c2266c4a70cb946879c0313854c780e152
Lucas Voboril <lucas.voboril@wolox.com.ar>
2018-06-01 09:12:53 -0300

    Pretty code, clean up repetitive tests

--- app/services/open_library_service.rb
+++ app/services/open_library_service.rb
@@ -2,16 +2,14 @@ class OpenLibraryService
   cattr_accessor :base_url_endpoint
   self.base_url_endpoint = 'https://openlibrary.org/'
 
-  def book_info(isbn) # rubocop:disable Metrics/MethodLength
+  def book_info(isbn)
     @isbn = isbn.to_s.rjust(10, '0')
 
-    response = HTTParty.get(self.class.base_url_endpoint + post_parameters(isbn), \
+    response = HTTParty.get(self.class.base_url_endpoint + post_parameters, \
                             headers: { 'Content-Type' => 'application/json' })
     case response.code
-    when 422
-      InvalidLibrary.new
     when 200..299
-      book_data_hash(JSON.parse(response))
+      book_data_hash(JSON.parse(response.body))
     else
       raise UnhandledResponseError
     end
@@ -21,23 +19,19 @@ class OpenLibraryService
 
   attr_accessor :isbn
 
-  def book_data_hash(response) # rubocop:disable AbcSize, Metrics/MethodLength
+  def book_data_hash(response) # rubocop:disable AbcSize
     raise ArgumentError, 'Incorrect ISBN' unless response.keys.include? "ISBN:#{isbn}"
-
     book_data = {}
     book_data[:isbn] = isbn
     book_data[:title] = response["ISBN:#{isbn}"]['title']
     book_data[:subtitle] = response["ISBN:#{isbn}"]['subtitle']
     book_data[:number_of_pages] = response["ISBN:#{isbn}"]['number_of_pages']
-    book_data[:authors] = if response["ISBN:#{isbn}"]['authors'].present?
-                            response["ISBN:#{isbn}"]['authors'].map { |author| author['name'] }.to_a # rubocop:disable Metrics/LineLength
-                          else
-                            []
-                          end
+    book_data[:authors] = response["ISBN:#{isbn}"]['authors']&.map { |a| a['name'] }&.compact
+    book_data[:authors] ||= [] # if not present, return empty array
     book_data
   end
 
-  def post_parameters(isbn)
+  def post_parameters
     "api/books?bibkeys=ISBN:#{isbn}&format=json&jscmd=data"
   end
 end
--- spec/services/open_library_service_spec.rb
+++ spec/services/open_library_service_spec.rb
@@ -3,56 +3,26 @@ require 'rails_helper'
 describe OpenLibraryService do
   response_path = './spec/fixtures/stubs_and_mocks/open_library_service_response_body.txt'
 
+  before do
+    stub_request(:any, %r{.*\/api\/books\?bibkeys=ISBN:.*&format=json&jscmd=data}) \
+      .to_return(body: File.open(response_path))
+  end
+
   describe '#book_info' do
     context 'With existent isbn' do
       existent_isbn = '0385472579'
-
-      it 'returns book isbn' do
-        stub_request(:any, %r{.*\/api\/books\?bibkeys=ISBN:.*&format=json&jscmd=data}) \
-          .to_return(body: File.open(response_path))
+      it 'returns book hash data' do
         o = described_class.new
         hash = o.book_info(existent_isbn)
-        expect(hash[:isbn]).to eq existent_isbn
-      end
-
-      it 'returns book title' do
-        stub_request(:any, %r{.*\/api\/books\?bibkeys=ISBN:.*&format=json&jscmd=data}) \
-          .to_return(body: File.open(response_path))
-        o = described_class.new
-        hash = o.book_info(existent_isbn)
-        expect(hash[:title]).to eq 'Zen speaks'
-      end
-
-      it 'returns book subtitle' do
-        stub_request(:any, %r{.*\/api\/books\?bibkeys=ISBN:.*&format=json&jscmd=data}) \
-          .to_return(body: File.open(response_path))
-        o = described_class.new
-        hash = o.book_info(existent_isbn)
-        expect(hash[:subtitle]).to eq 'shouts of nothingness'
-      end
-
-      it 'returns book number of pages' do
-        stub_request(:any, %r{.*\/api\/books\?bibkeys=ISBN:.*&format=json&jscmd=data}) \
-          .to_return(body: File.open(response_path))
-        o = described_class.new
-        hash = o.book_info(existent_isbn)
-        expect(hash[:number_of_pages]).to eq 159
-      end
-
-      it 'returns book authors' do
-        stub_request(:any, %r{.*\/api\/books\?bibkeys=ISBN:.*&format=json&jscmd=data}) \
-          .to_return(body: File.open(response_path))
-        o = described_class.new
-        hash = o.book_info(existent_isbn)
-        expect(hash[:authors]).to eq ['Zhizhong Cai']
+        expected_hash = { isbn: existent_isbn, title: 'Zen speaks', number_of_pages: 159, \
+                          subtitle: 'shouts of nothingness', authors: ['Zhizhong Cai'] }
+        expect(hash).to eq expected_hash
       end
     end
 
     context 'With invalid isbn' do
       invalid_isbn = '3854725221'
       it 'raise argumentError' do
-        stub_request(:any, %r{.*\/api\/books\?bibkeys=ISBN:.*&format=json&jscmd=data}) \
-          .to_return(body: File.open(response_path))
         o = described_class.new
         expect { o.book_info(invalid_isbn) }.to raise_error(ArgumentError)
       end
