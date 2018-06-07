b6c4a7b95ad9f6064d911c5eb6268d0f9b4896b3
Lucas Voboril <lucas.voboril@wolox.com.ar>
2018-06-07 08:57:17 -0300

    OK to create AdminUser in console

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# rails db:seed
 
 user = User.create(email: 'test@wolox.com.ar', password: '123123123', password_confirmation: '123123123', first_name: 'Test', last_name: 'TestLastName', locale: 'es')
 book = Book.create(genre: 'Drama',author: 'King', image: 'photo.png' ,title: 'It crew' ,publisher: 'editor', year: '2003')
 rent = Rent.create(user_id: 1, book_id: 1, from: Date.new(2018,5,7), to: Date.new(2018,5,9))
+admin = AdminUser.create!(email: 'admin@wolox.com.ar', password: '123123123', password_confirmation: '123123123') if Rails.env.development?
